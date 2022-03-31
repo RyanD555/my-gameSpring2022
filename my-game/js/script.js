529f65661a0879427fbdd9a27a490789a796e8d0
RyanD555 <Ryan.Deivert23@bcp.org>
2022-03-31 12:02:01 -0700

    Update script.js

--- my-game/js/script.js
+++ my-game/js/script.js
@@ -1,4 +1,4 @@
-class Square { //this seems similar to python's self
+class Sprite { //this seems similar to python's self
     constructor(x, y, w, h, speed, color) {
         this.x = x;
         this.y = y;
@@ -8,11 +8,13 @@ class Square { //this seems similar to python's self
         this.xVel = 1;
         this.yVel = 1;
         this.speed = speed
+        this.canBounce = true;
+        this.isBullet = false;
         allSprites.push(this);
     }
 
     create(x, y, w, h, speed, color) {
-        return new Square(x, y, w, h, speed, color); //ALWAYS make sure a constructor has all arguements. Creates a lot of problems
+        return new Sprite(x, y, w, h, speed, color); //ALWAYS make sure a constructor has all arguements. Creates a lot of problems
     }
 
     draw() {
@@ -24,26 +26,65 @@ class Square { //this seems similar to python's self
         this.x += this.xVel * this.speed;
         this.y += this.yVel * this.speed;
 
-        if (this.x > WIDTH - this.w) {
-            this.xVel = -1;
-        }
-
-        if (this.y > HEIGHT - this.h) {
-            this.yVel = -1;
-        }
-
-        if (this.x < 0) {
-            this.xVel = 1;
-        }
-
-        if (this.y < 0) {
-            this.yVel = 1
+        if (this.canBounce){ 
+            if (this.x > WIDTH - this.w) {
+                this.xVel = -1;
+            }
+    
+            if (this.y > HEIGHT - this.h) {
+                this.yVel = -1;
+            }
+    
+            if (this.x < 0) {
+                this.xVel = 1;
+            }
+    
+            if (this.y < 0) {
+                this.yVel = 1;
+            }
         }
     }
 }
 
-class Player {
+class Wall extends Sprite { //inheritance
     constructor(x, y, w, h, speed, color) {
+        super(x, y, w, h, speed, color);
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.speed = speed;
+        this.color = color;
+        allSprites.push(this);
+        allWalls.push(this);
+    }
+
+    create(x, y, w, h, color) {
+        return new Wall(x, y, w, h, 0, color);
+    }
+}
+
+class Cactus extends Sprite { //inheritance
+    constructor(x, y, w, h, speed, color) {
+        super(x, y, w, h, speed, color);
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.speed = speed;
+        this.color = "rgb(0, 255, 0)";
+        allSprites.push(this);
+        allWalls.push(this);
+    }
+
+    create(x, y, w, h) {
+        return new Cactus(x, y, w, h, 0, this.color);
+    }
+}
+
+class Player extends Sprite{
+    constructor(x, y, w, h, speed, color) {
+        super(x, y, w, h, speed, color);
         this.x = x;
         this.y = y;
         this.w = w;
@@ -52,6 +93,10 @@ class Player {
         this.speed = speed;
         this.xVel = 0;
         this.yVel = 0;
+        this.direction = "up";
+        this.coolDown = false;
+        this.coolTime = 10;
+        this.maxCoolTime = this.coolTime;
         allSprites.push(this);
     }
 
@@ -59,41 +104,91 @@ class Player {
         this.xVel = 0;
         this.yVel = 0;
         if ("a" in keysDown) {
+            this.direction = "left";
             this.xVel = -1;
         }
 
         if ("d" in keysDown) {
+            this.direction = "right";
             this.xVel = 1;
         }
         
         if ("w" in keysDown) {
+            this.direction = "up";
             this.yVel = -1;
         }
 
         if ("s" in keysDown) {
+            this.direction = "down";
             this.yVel += 1;
         }
 
-        if (this.x > WIDTH - this.w) {
-            this.xVel = -1;
+        if (this.x > WIDTH - this.w && "d" in keysDown) {
+            this.xVel = 0;
         }
 
-        if (this.y > HEIGHT - this.h) {
-            this.yVel = -1;
+        if (this.y > HEIGHT - this.h  - this.speed && "s" in keysDown) {
+            this.yVel = 0;
         }
 
-        if (this.x < 0) {
-            this.xVel = 1;
+        if (this.x < 0 && "a" in keysDown) {
+            this.xVel = 0;
         }
 
-        if (this.y < 0) {
-            this.yVel = 1
+        if (this.y < 0 && "w" in keysDown) {
+            this.yVel = 0;
+        }
+
+        if (" " in keysDown) {
+            this.shoot();
+        }
+
+        if ("c" in keysDown) { //clear all bullets from screen, super laggy if there are a lot of bullets because it is linear
+            for (i of allSprites) {
+                if (i instanceof Sprite) {
+                    if (i.isBullet) {
+                        i.x = 999999999; //get it the hell out of here
+                        i.y = 999999999;
+                        i.xVel = 0;
+                        i.yVel = 0;
+                    }
+                }
+            }
         }
     }
 
     draw() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+    shoot() { //problem is that one key press shoots 4 - 6 bullets
+        let bullet = new Sprite(this.x + (this.w / 2) - 30, this.y + (this.h / 2) - 30, 30, 30, 15, "rgb(" + randNum(0, 255) + "," + randNum(0, 255) + "," + randNum(0, 255) + ")"); //fix
+        bullet.canBounce = false;
+        bullet.isBullet = true;
+        bulletCount++;
+        if ("v" in keysDown) {
+            bullet.canBounce = !bullet.canBounce; //toggles if the bullet can bounce, must be held
+        }
+        switch (this.direction) {
+            case "up":
+                bullet.xVel = 0;
+                bullet.yVel = -1;
+                break;
+            case "down":
+                bullet.xVel = 0;
+                bullet.yVel = 1;
+                break;
+            case "left":
+                bullet.xVel = -1;
+                bullet.yVel = 0;
+                break;
+            case "right":
+                bullet.xVel = 1;
+                bullet.yVel = 0;
+                break;
+        }
+        return bullet;
     }
 
     update() {
@@ -148,6 +243,9 @@ class Circle {
 let canvas = null;
 let ctx = null;
 let allSprites = [];
+let allWalls = [];
+let allCacti = [];
+let bulletCount = 0;
 
 const WIDTH = 1024;
 const HEIGHT = 768;
@@ -159,10 +257,10 @@ const levelLayout = `
 .......#######..................
 ................................
 ..........................######
-#########..##########...........
-#########..##########...........
-#########..##########.......####
-#########..##########...........
+#########...#########...........
+#########...#########...........
+#########...#########...........
+#########...#########...........
 ................................
 .......................#########
 ................................
@@ -172,16 +270,17 @@ const levelLayout = `
 ................................
 ................................
 ...........#############........
+...........#############........
 ................................
 ................................
 ................................
-................................
-................................
-################################`//24 * 32 string grid
+..............C.................
+################################`//24 * 32 string grid, hold level layout
 
 const levelChars = { //literally just a python dictionary
     ".": "empty",
-    "#": Square,
+    "#": Wall,
+    "C": Cactus,
 };
 
 //keys
@@ -198,6 +297,13 @@ addEventListener("keyup", function(event) {
     delete keysDown[event.key];
     console.log(event);
 }, false);
+
+//Functions
+function randNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
 
 function update() {
     for (i of allSprites) { //now dont have to call every single object method
@@ -248,7 +354,7 @@ function readLevel(grid) {
                 } 
                 else {
                     let t = new type;
-                    startActors.push(t.create(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, 0, "rgb(0, 0, 255)"));
+                    startActors.push(t.create(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, "rgb(0, 0, 200)"));
                 }
             }
         }
@@ -282,9 +388,9 @@ function init() {
 readLevel(makeGrid(levelLayout, 32)); //make sure the width is right, this is perfect width
 
 //instances
-let square0 = new Square(0, 0, 100, 50, 5, "rgb(255, 255, 0)");
-let square1 = new Square(30, 20, 100, 50, 10, "rgb(255, 194, 194)");
-let square2 = new Square(400, 20, 100, 50, 7, "rgb(0, 200, 200)");
+let Sprite0 = new Sprite(0, 0, 100, 50, 5, "rgb(255, 255, 0)");
+let Sprite1 = new Sprite(30, 20, 100, 50, 10, "rgb(255, 194, 194)");
+let Sprite2 = new Sprite(400, 20, 100, 50, 7, "rgb(0, 200, 200)");
 let circle0 = new Circle(300, 300, 50, 90, 8, "rgb(255, 0, 0)");
 let circle1 = new Circle(500, 500, 30, 5, 20, "rgb(0, 0, 0)");
 let player = new Player(WIDTH / 2, HEIGHT / 2, 64, 64, 5, "rgb(0, 255, 0)");
